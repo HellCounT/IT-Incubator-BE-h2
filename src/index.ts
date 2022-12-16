3d59d576af8febb5ddc0849d8b698d180d8e19b1
HellCounT <a_killaheadz@live.ru>
2022-12-16 16:14:38 +0300

    Updated

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response} from "express"
-import {blogsDb} from "./repositories/blogs-repo"
+import {Blog, blogsDb} from "./repositories/blogs-repo"
 import {postsDb} from "./repositories/posts-repo"
 
 const app = express()
@@ -18,11 +18,27 @@ app.get('/api/blogs', (req: Request, res: Response) => {
 })
 
 app.post('/api/blogs', (req: Request, res: Response) => {
+    // Blog adding
+    const dateNow = new Date()
+    const addBlog: Blog = {
+        id: +dateNow,
+        name: req.body.name,
+        description: req.body.description,
+        websiteUrl: req.body.websiteUrl,
+    }
+    blogsDb.push(addBlog)
 
+    return res.status(201).send(addBlog)
 })
 
 app.get('/api/blogs/:id', (req: Request, res: Response) => {
-
+    const blogId: number = +req.params.id
+    const foundBlog = blogsDb.find(b => (b.id === blogId))
+    if (foundBlog) {
+        res.status(200).send(foundBlog)
+    } else {
+        res.sendStatus(404)
+    }
 })
 
 app.put('/api/blogs/:id', (req: Request, res: Response) => {
@@ -43,7 +59,13 @@ app.post('/api/posts', (req: Request, res: Response) => {
 })
 
 app.get('/api/posts/:id', (req: Request, res: Response) => {
-
+    const postId: string = req.params.id
+    const foundPost = postsDb.find(p => (p.id === postId))
+    if (foundPost) {
+        res.status(200).send(foundPost)
+    } else {
+        res.sendStatus(404)
+    }
 })
 
 app.put('/api/posts/:id', (req: Request, res: Response) => {
--- src/repositories/posts-repo.ts
+++ src/repositories/posts-repo.ts
@@ -1,28 +1,36 @@
 export type Post = {
+    id: string
     title: string,
     shortDescription: string,
     content: string,
     blogId: number
+    blogName: string
 }
 
 export const postsDb: Array<Post> = [
     {
+        id: "1",
         title: "Funny Cats",
         shortDescription: "Some text about cats",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu.",
-        blogId: 1
+        blogId: 1,
+        blogName: "Our Pets Blog"
     },
     {
+        id: "2",
         title: "Rock Am Ring 2001",
         shortDescription: "How it was at RaR2001",
         content: "Urna et pharetra pharetra massa massa ultricies. Arcu odio ut sem nulla pharetra diam sit. Eros donec ac odio tempor orci. Est ante in nibh mauris cursus mattis molestie a iaculis.",
-        blogId: 2
+        blogId: 2,
+        blogName: "Rock Concerts"
     },
     {
+        id: "3",
         title: "Qlimax 2022",
         shortDescription: "My report from Q2022",
         content: "Luctus accumsan tortor posuere ac ut consequat semper. Mi tempus imperdiet nulla malesuada. Pretium quam vulputate dignissim suspendisse in est ante in nibh.",
-        blogId: 3
+        blogId: 3,
+        blogName: "Rave Music Digest"
     }
 ]
 
