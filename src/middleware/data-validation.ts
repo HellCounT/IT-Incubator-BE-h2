5d8df371bfd11397cbcf52f3696776d07dbb7281
HellCounT <a_killaheadz@live.ru>
2022-12-19 21:26:18 +0300

    upd

--- src/middleware/data-validation.ts
+++ src/middleware/data-validation.ts
@@ -4,10 +4,12 @@ import {validationResult} from "express-validator";
 export const inputValidation = (req: Request, res: Response, next: NextFunction) => {
     const errorMessages = validationResult(req).array({onlyFirstError: true})
     if (errorMessages.length > 0) {
-        res.status(400).send(errorMessages.map(e => ({
-            message: e.msg,
-            field: e.location
-        })))
+        res.status(400).send({
+                "errorMessages": errorMessages.map(e => ({
+                    message: e.msg,
+                    field: e.location
+                }))}
+            )
     } else {
         next()
     }
