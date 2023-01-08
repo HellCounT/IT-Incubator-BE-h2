d10e5877d766d44894e7aa6af60c54ebf1ba1593
HellCounT <a_killaheadz@live.ru>
2023-01-08 17:22:28 +0300

    updated deletion of all data

--- src/routes/delete-all-router.ts
+++ src/routes/delete-all-router.ts
@@ -1,10 +1,11 @@
 import {Request, Response, Router} from "express";
-import {blogsCollection, postsCollection} from "../repositories/db";
+import {blogsCollection, postsCollection, usersCollection} from "../repositories/db";
 
 export const deleteAllRouter = Router({})
 
 deleteAllRouter.delete('/all-data', async (req: Request, res: Response) => {
     await blogsCollection.deleteMany({})
     await postsCollection.deleteMany({})
+    await usersCollection.deleteMany({})
     res.sendStatus(204)
 })
