bf5672932333b9e4d620adc355598b5a0e281360
HellCounT <a_killaheadz@live.ru>
2023-01-08 18:13:32 +0300

    auth validator fix

--- src/domain/users-service.ts
+++ src/domain/users-service.ts
@@ -1,7 +1,8 @@
 import {UserViewType} from "../repositories/queryRepo";
-import {UserCreateType} from "../repositories/db";
+import {UserCreateType, UserInsertDbType} from "../repositories/db";
 import bcrypt from 'bcrypt'
 import {usersRepo} from "../repositories/users-database";
+import {WithId} from "mongodb";
 
 export const usersService = {
     async createUser(login: string, password: string, email: string): Promise<UserViewType> {
@@ -18,10 +19,13 @@ export const usersService = {
         return await usersRepo.deleteUser(id)
     },
     async checkCredentials(loginOrEmail: string, password: string): Promise<boolean> {
-        const user = await usersRepo.findByLoginOrEmail(loginOrEmail)
+        const user: WithId<UserInsertDbType> | null = await usersRepo.findByLoginOrEmail(loginOrEmail)
         if (!user) return false
-        const passwordHash = await this._generateHash(password)
-        return user.hash === passwordHash;
+        else {
+            const passwordHash = await usersService._generateHash(password)
+            return user.hash === passwordHash;
+        }
+
     },
     async _generateHash(password: string) {
         return await bcrypt.hash(password, await bcrypt.genSalt(10))
