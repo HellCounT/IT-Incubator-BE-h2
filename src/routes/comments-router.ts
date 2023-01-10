e103ecfd0da7d1b064c3113a43204ad0d678fc5e
HellCounT <a_killaheadz@live.ru>
2023-01-10 14:34:21 +0300

    upd comments-router.ts

--- src/routes/comments-router.ts
+++ src/routes/comments-router.ts
@@ -1,11 +1,14 @@
 import {Request, Response, Router} from "express";
 import {commentsService} from "../domain/comments-service";
 import {commentDataValidator, inputValidation} from "../middleware/data-validation";
+import {commentsQueryRepo} from "../repositories/queryRepo";
 
 export const commentsRouter = Router({})
 
 commentsRouter.get('/:id', async (req: Request, res: Response) => {
-
+    const commentIdSearchResult = await commentsQueryRepo.findCommentById(req.params.id)
+    if (commentIdSearchResult) return res.status(200).send(commentIdSearchResult)
+    else res.sendStatus(404)
 })
 
 commentsRouter.put('/:commentId',
